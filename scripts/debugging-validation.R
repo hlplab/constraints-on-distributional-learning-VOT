6648e37da67f2a98e1d5277767c60e37463b9131
tfjaeger <fjaeger@ur.rochester.edu>
2023-06-01 14:38:04 -0400

    code revisions

--- scripts/debugging-validation.R
+++ scripts/debugging-validation.R
@@ -1,17 +1,17 @@
-add_epred_draws(d_for_analysis %>% 
-                    filter(Phase == "test") %>% 
-                    prepVars(levels.Condition = levels_Condition.Exposure, contrast_type = "difference") %>% 
-                    distinct(Block, Condition.Exposure) %>% 
-                    mutate(VOT_gs = 0), 
-                  fit_mix_uniform_bias,
+add_epred_draws(d_for_analysis %>%
+                    filter(Phase == "test") %>%
+                    prepVars(levels.Condition = levels_Condition.Exposure, contrast_type = "difference") %>%
+                    distinct(Block, Condition.Exposure) %>%
+                    mutate(VOT_gs = 0),
+                  fit_mix_test,
                   ndraws = 4000, re_formula = NA) %>% group_by(Block, Condition.Exposure) %>% summarise(mean(.epred)) -> x
 x
 
 
-add_epred_draws(d_for_analysis %>% 
-                    filter(Phase == "test") %>% 
-                    prepVars(levels.Condition = levels_Condition.Exposure, contrast_type = "helmert") %>% 
-                    distinct(Block, Condition.Exposure) %>% 
+add_epred_draws(d_for_analysis %>%
+                    filter(Phase == "test") %>%
+                    prepVars(levels.Condition = levels_Condition.Exposure, contrast_type = "helmert") %>%
+                    distinct(Block, Condition.Exposure) %>%
                     mutate(VOT_gs = 0), fit_mix_helmert,
                   ndraws = 4000, re_formula = NA) %>% group_by(Block, Condition.Exposure) %>% summarise(mean(.epred)) -> y
 
@@ -19,7 +19,7 @@ y
 x
 # minimal example to validate sliding difference coding effects
 nlevel <- 3
-d <- 
+d <-
   crossing(
     Block = factor(1:nlevel),
     Condition = factor(LETTERS[1:nlevel]))
@@ -33,27 +33,27 @@ X <- model.matrix(~ Block * Condition, data = d)
 beta <- c(1:9)
 # this multiplies the contrast design, X with the vector of regression coefficients (where the first number represents the coeff for the intercept)
 # %*% is the matrix multiplication operator or dot product
-d$y <- X %*% beta 
+d$y <- X %*% beta
 
 # Cell means by Block (from which we can infer the main effects of Block)
-d %>% 
+d %>%
   group_by(Block) %>%
   summarise(mean(y))
 
 # Cell means by Condition (from which we can infer the main effects of Condition)
-d %>% 
+d %>%
   group_by(Condition) %>%
   summarise(mean(y))
 
 # Interaction between Block 2vs1 and Condition BvsA
-d %>% 
+d %>%
   filter(Block %in% c(1, 2), Condition %in% c("A", "B"))
 # In Block 1, Cond B vs. A = -4.67
 # In Block 2, Cond B vs. A = +7.33
 # Interaction = 6
 
 # Interaction between Block 3vs2 and Condition BvsA
-d %>% 
+d %>%
   filter(Block %in% c(2, 3), Condition %in% c("A", "B"))
 # In Block 2, Cond B vs. A = 3.663
 # In Block 3, Cond B vs. A = 10.67
@@ -61,7 +61,7 @@ d %>%
 
 
 # Interaction between Block 3vs1 and Condition BvsA
-d %>% 
+d %>%
   filter(Block %in% c(1, 3), Condition %in% c("A", "B"))
 # In Block 1, Cond B vs. A = -2.33
 # In Block 3, Cond B vs. A = 10.67
@@ -70,7 +70,7 @@ d %>%
 
 
 # Interaction between Block 3vs2 and Condition CvsA
-d %>% 
+d %>%
   filter(Block %in% c(3, 1), Condition %in% c("A", "C"))
 # In Block 1, Cond C vs. A = -5.66
 # In Block 3, Cond C vs. A = 24.3
@@ -78,7 +78,7 @@ d %>%
 
 
 # Interaction between Block 2vs1 and Condition CvsA
-d %>% 
+d %>%
   filter(Block %in% c(1, 2), Condition %in% c("A", "C"))
 # In Block 1, Cond C vs. A = -5.66
 # In Block 2, Cond C vs. A = 8.33
@@ -87,22 +87,22 @@ d %>%
 
 # Simple effect coding
 X <- model.matrix(~ Block / Condition, data = d)
-beta <- c(1:9) * 2 
+beta <- c(1:9) * 2
 d$y <- X %*% beta
 
 # Cell means by Block (from which we can infer the main effects of Block)
-d %>% 
+d %>%
   group_by(Block) %>%
   summarise(mean(y))
 
 
 # Cell means by Condition (from which we can infer the main effects of Condition)
-d %>% 
+d %>%
   group_by(Condition) %>%
   summarise(mean(y))
 
 # Interaction between Block 2vs1 and Condition BvsA
-d %>% 
+d %>%
   filter(Block %in% c(1, 2), Condition %in% c("A", "B"))
 # In Block 1, Cond B vs. A = +4.67
 # In Block 2, Cond B vs. A = +7.33
