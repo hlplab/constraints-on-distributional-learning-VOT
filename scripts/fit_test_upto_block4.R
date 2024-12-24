cddfd60f10f88bf75d0f713fada3870c461f69b6
Maryann <maryann.tan@biling.su.se>
2024-12-24 11:11:04 +1100

    saved psychometric model up to test 4; IA logodds model

--- scripts/fit_test_upto_block4.R
+++ scripts/fit_test_upto_block4.R
@@ -6,15 +6,19 @@ library(magrittr)
 d.IA_predicted_logodds <- 
   read_csv("/home/mtan12/AEDLVOT/data/d.IA_predicted_logodds.csv", show_col_types = FALSE)
 
-# code Block as factor
+VOT.mean_test_upto_block4 <- mean(d.IA_predicted_logodds$Item.VOT)
+VOT.sd_test_upto_block4 <- sd(d.IA_predicted_logodds$Item.VOT)
+
 d.IA_predicted_logodds %<>%
   ungroup() %>% 
   droplevels() %>%
-  mutate(Block = factor(Block),
-         VOT_gs = Item.VOT - VOT.mean_test / (2 * VOT.sd_test))
+  mutate(across(c(Block, Condition.Exposure), factor),
+         VOT_gs = Item.VOT - VOT.mean_test_upto_block4 / (2 * VOT.sd_test_upto_block4))
 
 contrasts(d.IA_predicted_logodds$Block) <- MASS::fractions(MASS::contr.sdif(4))
-dimnames(contrasts(d.IA_predicted_logodds$Block))[[2]] <- c("_Block2 vs. Block1", "_Block3 vs. Block2", "_Block4 vs. Block3")
+dimnames(contrasts(d.IA_predicted_logodds$Block))[[2]] <- c("_Test2 vs. Test1", "_Test3 vs. Test2", "_Test4 vs. Test3")
+contrasts(d.IA_predicted_logodds$Condition.Exposure) <- cbind("_Shift10 vs. Shift0" = c(-2/3, 1/3, 1/3),
+                                                              "_Shift40 vs. Shift10" = c(-1/3,-1/3, 2/3))
 
 my_priors <-  c(
   prior(student_t(3, 0, 2.5), class = "b", dpar = "mu2"),
@@ -24,7 +28,7 @@ my_priors <-  c(
 
 fit_test.upto.block4 <- 
   brm(
-    formula = bf(Response.Voiceless ~ 1,
+    bf(Response.Voiceless ~ 1,
                  mu1 ~ 0 + offset(0),
                  mu2 ~ 1 + VOT_gs * Condition.Exposure * Block + 
                    (1 + VOT_gs * Block | ParticipantID) + (1 + VOT_gs * Condition.Exposure * Block | Item.MinimalPair),
